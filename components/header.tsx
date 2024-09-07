b299b94088a05d2094517406a0573a3eaddb018e
Pamudu Chathunka <119758027+pamuduchat@users.noreply.github.com>
2024-09-07 13:26:28 +0530

    Implement header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,8 @@
 import { HeaderLogo } from "@/components/header-logo";
+import { Navigation } from "@/components/navigation";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import { WelcomeMsg } from "@/components/welcome-msg";
 
 export const Header = () => {
   return (
@@ -7,8 +11,16 @@ export const Header = () => {
         <div className="w-full flex items-center justify-between mb-14">
           <div className="flex items-center lg:gap-x-16">
             <HeaderLogo />
+            <Navigation />
           </div>
+          <ClerkLoaded>
+            <UserButton />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="size-8 animate-spin text-slate-400" />
+          </ClerkLoading>
         </div>
+        <WelcomeMsg />
       </div>
     </header>
   );
