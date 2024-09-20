915401537c7f672f6d3ff3e3fea0fe32fd5581c0
Pamudu Chathunka <119758027+pamuduchat@users.noreply.github.com>
2024-09-20 23:51:23 +0530

    Refactor account table columns and integrate with API response

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -9,28 +9,36 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { Plus } from "lucide-react";
-import { columns, Payment } from "./columns";
-import { DataTable } from "@/components/data-table";
+import { useGetAccounts } from "@/features/accounts/api/use_get_accounts";
 
-const data: Payment[] = [
-  {
-    id: "728ed52f",
-    amount: 100,
-    status: "pending",
-    email: "m@example.com",
-  },
-  {
-    id: "72342d52f",
-    amount: 200,
-    status: "success",
-    email: "a@example.com",
-  },
-  // ...
-];
+import { Loader2, Plus } from "lucide-react";
+import { columns } from "./columns";
+import { DataTable } from "@/components/data-table";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const data = [];
 
 const AccountsPage = () => {
   const newAccount = useNewAccount();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || [];
+
+  if (accountsQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+            <Skeleton className="h-8 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -45,7 +53,7 @@ const AccountsPage = () => {
         <CardContent>
           <DataTable
             columns={columns}
-            data={data}
+            data={accounts}
             filterKey="email"
             onDelete={() => {}}
             disabled={false}
